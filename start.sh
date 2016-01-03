610b546ff0651bd0ff89b8261b2d232ee4bc52c4
watsy0007 <watsy0007@gmail.com>
2016-01-03 09:41:15 +0800

    update

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@ if [ $RAILS_ENV != "production" ]; then
     echo "Skip in daocloud.io's build server" 1>&2
     exit 0
 fi
-bundle exec rake install
 bundle exec rake db:migrate
 
 pumactl -F config/puma.rb start
